42c35312ce92d4b4ddb19873aa1195f236c921f0
Jesus Parra <jesus.parra@outlook.com>
2018-09-29 11:09:21 -0500

    Hola mundo

---
+++ Test.c
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+ Name        : Test.c
+ Author      : TT3
+ Version     :
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!");
+	return 1;
+}
