23b0c8c601d46eebb2f64a19f981618278a62e88
Jesus Parra <jesus.parra@outlook.com>
2018-09-29 15:54:16 -0500

    Configuracion

--- gpio_config.c
+++ gpio_config.c
@@ -1,35 +1,36 @@
-//#include <unistd.h>
 #include <stdio.h>
-//using namespace std;
+#include <stdlib.h>
 
 int main()
 {
    FILE *export_file = NULL; //declare pointers
+   FILE *gpmc_ad4= NULL; //declare pointers
    FILE *IO_direction = NULL;
    FILE *IO_value = NULL;
    char zero[] = "0";
    char uno[] = "1";
+   char diescisiete[] = "17";
    char direction[] = "in";
-   char gpio[] = "23";
-   //this part here exports gpio23
+   char gpio[] = "36";
+   
+   //this part here exports gpio36
+   //gets(export_file);
    export_file = fopen ("/sys/class/gpio/export", "w");
    fwrite (gpio, 1, sizeof(gpio), export_file);
    fclose (export_file);
+   
+   gpmc_ad4 = fopen ("/sys/kernel/debug/omap_mux/gpmc_ad4", "w");
+   fwrite (diescisiete, 1, sizeof(diescisiete), gpmc_ad4);
+   fclose (gpmc_ad4);
+   
    //this part here sets the direction of the pin
-   IO_direction = fopen("/sys/class/gpio/gpio23/direction", "w");
+   //gets(IO_direction);
+   IO_direction = fopen("/sys/class/gpio/gpio36/direction", "w");
    fwrite(direction, 1, sizeof(direction), IO_direction); //set the pin to HIGH
    fclose(IO_direction);
-   //usleep (1000000);
- 
-   //for (int i=0; i<10; i++){ //blink LED 10 times
-        IO_value = fopen ("/sys/class/gpio/gpio23/value", "w");
-        fwrite (uno, 1, sizeof(uno), IO_value); //set the pin to HIGH
-        fclose (IO_value);
-        //usleep (1000000); //delay for a second
-
-        //IO_value = fopen ("/sys/class/gpio/gpio23/value", "w");
-        //fwrite (zero, 1, sizeof(zero), IO_value); //set the pin to LOW
-        //fclose (IO_value);
-       //usleep (1000000); //delay for a second
-      //  }
+   
+   //gets(IO_value);
+   IO_value = fopen ("/sys/class/gpio/gpio36/value", "w");
+   fwrite (uno, 1, sizeof(uno), IO_value); //set the pin to HIGH
+   fclose (IO_value);
 }
