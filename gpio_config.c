5aeb031ec6b1079b31c8308375cf5027e1a304a2
Jesus Parra <jesus.parra@outlook.com>
2018-09-29 17:44:01 -0500

    test

--- gpio_config.c
+++ gpio_config.c
@@ -3,12 +3,13 @@
 
 int main()
 {
+	printf("Declarando variables");
    FILE *export_file = NULL; //declare pointers
-   FILE *gpmc_ad4= NULL; //declare pointers
+   //FILE *gpmc_ad4= NULL; //declare pointers
    FILE *IO_direction = NULL;
    char zero[] = "0";
    char uno[] = "1";
-   char diescisiete[] = "17";
+   //char diescisiete[] = "17";
    char direction[] = "in";
    char gpio[] = "36";
    
@@ -18,26 +19,23 @@ int main()
    
    //this part here exports gpio36
    //gets(export_file);
+   printf("Modificando export");
    export_file = fopen ("/sys/class/gpio/export", "w");
    fwrite (gpio, 1, sizeof(gpio), export_file);
    fclose (export_file);
    
    //this part here sets the direction of the pin
-   //gets(IO_direction);
+   printf("Modificando direction");
    IO_direction = fopen("/sys/class/gpio/gpio36/direction", "w");
    fwrite(direction, 1, sizeof(direction), IO_direction); //set the pin to HIGH
    fclose(IO_direction);
    
    //Read Value:
+   printf("Leyendo valor");
     char c[1];
     FILE *fptr;
 
-    if ((fptr = fopen("/sys/class/gpio/gpio36/value", "r")) == NULL)
-    {
-        printf("Error! opening file");
-        // Program exits if file pointer returns NULL.
-        exit(1);         
-    }
+    fptr = fopen("/sys/class/gpio/gpio36/value", "r");
 
     // reads text until newline 
     fscanf(fptr,"%[^\n]", c);
