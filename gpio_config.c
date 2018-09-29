b5b042a7170a6f6d01588826eb5275cc9a598713
Jesus Parra <jesus.parra@outlook.com>
2018-09-29 15:59:38 -0500

    correcciones

--- gpio_config.c
+++ gpio_config.c
@@ -19,18 +19,18 @@ int main()
    fwrite (gpio, 1, sizeof(gpio), export_file);
    fclose (export_file);
    
-   gpmc_ad4 = fopen ("/sys/kernel/debug/omap_mux/gpmc_ad4", "w");
+   /* gpmc_ad4 = fopen ("/sys/kernel/debug/omap_mux/gpmc_ad4", "w");
    fwrite (diescisiete, 1, sizeof(diescisiete), gpmc_ad4);
-   fclose (gpmc_ad4);
+   fclose (gpmc_ad4); */
    
    //this part here sets the direction of the pin
    //gets(IO_direction);
-   IO_direction = fopen("/sys/class/gpio/gpio36/direction", "w");
+   /* IO_direction = fopen("/sys/class/gpio/gpio36/direction", "w");
    fwrite(direction, 1, sizeof(direction), IO_direction); //set the pin to HIGH
-   fclose(IO_direction);
+   fclose(IO_direction); */
    
    //gets(IO_value);
-   IO_value = fopen ("/sys/class/gpio/gpio36/value", "w");
+/*    IO_value = fopen ("/sys/class/gpio/gpio36/value", "w");
    fwrite (uno, 1, sizeof(uno), IO_value); //set the pin to HIGH
-   fclose (IO_value);
+   fclose (IO_value); */
 }
