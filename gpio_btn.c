3c75eaafc60edd4b2804487c4b0644fa8f6ccba8
Jesus Parra <jesus.parra@outlook.com>
2018-09-29 19:08:16 -0500

    test

--- gpio_btn.c
+++ gpio_btn.c
@@ -33,42 +33,42 @@ int main(void)
             exit(1);
         } 
 
-    status = access("/sys/class/gpio/gpio45/value", F_OK ); 
+/*     status = access("/sys/class/gpio/gpio45/value", F_OK ); 
     if (status == -1)
         {
             //file doesnt exist
             printf("GPIO_45 file doesnt exist dude\n");
             exit(1);
-        }
+        } */
 
 
     //set GPIO 45 as output
     system("echo in > /sys/class/gpio/gpio44/direction");
-    system("echo out > /sys/class/gpio/gpio45/direction");
+    //system("echo out > /sys/class/gpio/gpio45/direction");
     sleep(1);
 
     char val[10];
 
-	strcpy(val,"1\n");
+	//strcpy(val,"1\n");
     while(1)
         {
-            //GPIOREAD("/sys/class/gpio/gpio44/value", val);
+            GPIOREAD("/sys/class/gpio/gpio44/value", val);
             printf("BUTTON STAT %s\n", val);
 
             if(!strcmp(val,"1\n"))
             {
-                printf("%u) AAA LED --- ON\n", cnt);
+                printf("%u) ON\n", cnt);
                 //system("echo 1 > /sys/class/gpio/gpio45/value");
             }
             else
             {
-                printf("%u) AAA LED --- OFF\n", cnt);
+                printf("%u) OFF\n", cnt);
                 //system("echo 0 > /sys/class/gpio/gpio45/value");
             }
 			
-			if(!strcmp(val,"1\n")){
+			/* if(!strcmp(val,"1\n")){
 				strcpy(val,"0\n");
-			} else {strcpy(val,"1\n");}
+			} else {strcpy(val,"1\n");} */
         }
 
     return 0;
